00b04a2d1b6474c9eeb3b6a895a583122c232b1f
Chua Hou <human+github@chuahou.dev>
2021-06-13 14:08:09 +0800

    CFGEq.CFG: remove .->, .|, .>
    - Postfix .> doesn't work without parentheses.

--- src/CFGEq/CFG.hs
+++ src/CFGEq/CFG.hs
@@ -17,7 +17,6 @@
 module CFGEq.CFG where
 
 import           Data.Set (Set)
-import qualified Data.Set as Set
 
 -- | @CFG v t@ is the type of context-free grammars with variables of type @v@
 -- and terminals of type @t@. Each such grammar contains the set of 'Rule's
@@ -41,20 +40,3 @@ data Rule v t =
 -- | The type of productions in a 'Rule'. The string is a list of 'Either's,
 -- with 'Left's representing variables and 'Right's representing terminals.
 type Production v t = [Either v t]
-
--- | Acts like \(\to\) in normal CFGs for use with '.|' and '.>'. To write
--- \(S \to AB \mid \epsilon\), for example, write
---
--- > S .-> [Left A, Left B] .| [] .>
-(.->) :: (Ord v, Ord t) => v -> Set (Production v t) -> Set (Rule v t)
-(.->) s = Set.map (s :->)
-
--- | Acts like \(\mid\) in normal CFGs for use with '.->' and '.>'.
-(.|) :: (Ord v, Ord t)
-     => Production v t -> Set (Production v t) -> Set (Production v t)
-(.|) = Set.insert
-
--- | Terminates a list of alternatives using '.->' and '.|'. Necessary so that
--- such expressions are well typed.
-(.>) :: (Ord v, Ord t) => Production v t -> Set (Production v t)
-(.>) = Set.singleton
